2a3f9a9e30f9fab5acda66ec64087afcdc4f4c20
Sona Mol Mathew <sonamol.teresa@gmail.com>
2017-11-21 00:22:40 +0530

    modified helloworld

--- day1/src/helloworld/HelloWorld.java
+++ day1/src/helloworld/HelloWorld.java
@@ -3,5 +3,6 @@ package helloworld;
 public class HelloWorld {
     public static void main(String []args){
         System.out.println("Hello World Sona");
+        System.out.println("Hello World Sandeep");
     }
 }
